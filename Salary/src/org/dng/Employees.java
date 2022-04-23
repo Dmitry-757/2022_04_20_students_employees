96e1e60d1b18da4600265a5de1e3ad668bcb9eea
Dmitry757 <glv11@mail.ru>
2022-04-24 02:10:50 +0300

    На основе предыдущего задания переделать отчет, с учетом того, что сотрудники с почасовой оплатой, половину зарплаты получают в валюте (тугриках), по курсу на день начисления заработной платы.

--- Salary/src/org/dng/Employees.java
+++ Salary/src/org/dng/Employees.java
@@ -6,6 +6,7 @@ public class Employees {
     private PaymentType paymentType;
     private double paymentValue;
     private boolean hasChildren = false; //for task 3
+    private static double currencyRate = 0; //for task 4
 
     public Employees(String surname, PaymentType paymentType, double paymentValue) {
         this.surname = surname;
@@ -24,6 +25,9 @@ public class Employees {
 
     public String getSurname() {
         return surname;
+    }
+    public void setSurname(String name) {
+        this.surname = name;
     }
 
     public PaymentType getPaymentType() {
@@ -49,13 +53,34 @@ public class Employees {
     }
     public double getSalaryValue() {
         //childless discrimination ((
-        return paymentValue * (100 - getTaxRate() ) / 100;
+        double salaryVal = 0;
+        //for first 3 tasks condition must provide calculating of salary value without currency
+        if ((paymentType == PaymentType.HOURLY)&&(currencyRate != 0 )){
+            salaryVal = paymentValue * (100 - getTaxRate() ) / 100 / 2;
+        }
+        else{
+            salaryVal = paymentValue * (100 - getTaxRate() ) / 100;
+        }
+        return salaryVal;
+    }
+    public double getCurrencySalaryValue() {
+        //discrimination of childless employees ((
+        double salaryVal = 0;
+        if (paymentType == PaymentType.HOURLY){
+            salaryVal = paymentValue * (100 - getTaxRate() ) / 100 / 2 / currencyRate;
+        }
+
+        return salaryVal;
     }
 
     //** unused **
 //    public String getSummary() {
 //        return "Employee: " + surname + "  paymentType: " + paymentType + "  paymentValue: " + paymentValue + "(tugrik(s))  TaxRate(%):" + paymentType.getTaxRate();
 //    }
+
+    public static void setCurrencyRate(double currencyR) {
+        currencyRate = currencyR;
+    }
 
     //** Summary for task1
     public void printSummary1() {
@@ -64,7 +89,12 @@ public class Employees {
 
     //** Summary for task2 & task3
     public void printSummary2() {
-        System.out.printf("%10s %11d %14.2f %11s  \n", getSurname(), getTaxRate(), getPaymentValue(), getSalaryValue());
+        System.out.printf("%10s %11d %14.2f %11.2f  \n", getSurname(), getTaxRate(), getPaymentValue(), getSalaryValue());
+    }
+
+    //** Summary for task4
+    public void printSummary4() {
+        System.out.printf("%10s %11d %14.2f   (%8.2f /%7.2f)   \n", getSurname(), getTaxRate(), getPaymentValue(), getSalaryValue(), getCurrencySalaryValue());
     }
 
 }
--- Salary/src/org/dng/Salary.java
+++ Salary/src/org/dng/Salary.java
@@ -8,6 +8,7 @@ public class Salary {
 
 
         //task 1
+        System.out.println("Task 1");
         System.out.printf("%10s %12s %13s %12s \n", "Surname", "PaymentType", "PaymentValue", "Tax rate(%)");
         employee1.printSummary1();
         employee2.printSummary1();
@@ -15,6 +16,7 @@ public class Salary {
         System.out.println();
 
         //task 2
+        System.out.println("Task 2");
         //Surname, TaxRate, PaymentValue, SalaryValue
         System.out.printf("%10s %12s %13s %11s \n", "Surname", "TaxRate(%)", "PaymentValue", "SalaryValue");
         employee1.printSummary2();
@@ -23,6 +25,7 @@ public class Salary {
         System.out.println();
 
         //task 3
+        System.out.println("Task 3");
         Employees employee4 = new Employees("Ivanov_3", PaymentType.RATE, 300, true);
         Employees employee5 = new Employees("Sidorov_3", PaymentType.HOURLY, 800, false);
         Employees employee6 = new Employees("Petrov_3", PaymentType.PIECEWORK, 5500, false);
@@ -30,6 +33,26 @@ public class Salary {
         employee4.printSummary2();
         employee5.printSummary2();
         employee6.printSummary2();
+        System.out.println();
+
+        //task 4
+        /*
+        На основе предыдущего задания переделать отчет, с учетом того, что сотрудники с почасовой оплатой, половину
+        зарплаты получают в валюте (тугриках), по курсу на день
+        начисления заработной платы
+         */
+        System.out.println("Task 4");
+        employee4.setSurname("Ivanov_4");
+        employee5.setSurname("Sidorov_4");
+        employee6.setSurname("Petrov_4");
+        System.out.printf("%10s %12s %13s %20s \n", "Surname", "TaxRate(%)", "PaymentValue", "SalaryValue (grn/tugr)");
+        Employees.setCurrencyRate(10);
+
+        employee4.printSummary4();
+        employee5.printSummary4();
+        employee6.printSummary4();
+        System.out.println();
+
 
 
     }
